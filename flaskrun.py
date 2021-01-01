76e16f82c4b6a0033decf186c63dbd039124d20c
awaisgt <39981782+awaisgt@users.noreply.github.com>
2021-01-02 02:10:00 +0500

    Create flaskrun.py

---
+++ flaskrun.py
@@ -0,0 +1,27 @@
+import myselenium
+from flask import Flask, redirect, url_for, render_template, request,session
+app= Flask(__name__)
+@app.route('/')
+def my_form():
+    return render_template('form.html')
+
+@app.route('/', methods=['POST','GET'])
+def my_form_post():
+    if request.method == 'POST':
+        link = request.form['username']
+        email = request.form['password']
+        minutes = request.form['minutes']
+        file = open("abc.txt",'w')
+        strx = link+ "\n" + email+"\n"+minutes
+        file.write(strx)
+        myselenium(link,email,minutes)
+        return render_template('show.html', username=link ,password = email ,minutes = minutes, txt = "You will soon receive the confirmation email." )
+
+
+@app.route('/show')
+def my_formx():
+    file = open("abc.txt",'r')
+    first = file.readline()
+    second = file.readline()
+    minutes = file.readline()
+    return render_template('show.html', username=first ,password = second ,minutes=minutes, txt ="")
